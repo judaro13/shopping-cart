73b1d5cdeae2f9dfb66f1958db40a72921834f43
Juliana Davila <judaro13@gmail.com>
2015-06-24 10:54:30 -0500

    adding layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :day_deals
+
+  def day_deals
+    @deals = {'panasonic': {url: 'products/panasonic.jpg', price: 400, alt: 'Bootshop panasonoc New camera'}, 'kindle': {url: 'products/kindle.png',  price: 200, alt: 'Bootshop Kindel'}}
+
+  end
+
 end
