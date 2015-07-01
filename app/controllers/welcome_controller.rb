0321853de35fcef44774e56b776d96d1e543a047
Juliana Davila <judaro13@gmail.com>
2015-06-30 22:35:44 -0500

    added welcome controller for shopping cart index instead shopping cart controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class  WelcomeController < ApplicationController
+  def index
+    @feature_products = Item.all.limit(16)
+    @lasted_products = Item.all.asc.limit(16)
+    render layout: 'welcome'
+  end
+end
